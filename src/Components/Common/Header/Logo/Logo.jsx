9b2c31750b53562dab2c21b3a4234a6846cf98d3
Théo_CATALA <93990201+tctla@users.noreply.github.com>
2023-12-04 12:41:31 +0100

    First commit

---
+++ src/Components/Common/Header/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import"./PokemonList.css";
+
+const Logo = () => {
+  return (
+    <header>
+        <h1 className='logo' >Logo</h1>
+    </header>
+  );
+};
+
+export default Logo;
---
+++ src/Components/PokemonList/PokemonList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import"./PokemonList.css";
+
+const PokemonList = () => {
+  return (
+    <header>
+        <h1 className='pokemonlist' >Pokemon List</h1>
+    </header>
+  );
+};
+
+export default PokemonList;
