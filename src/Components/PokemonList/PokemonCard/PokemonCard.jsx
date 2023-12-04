301450faa4a50aac1fab5de48ba0f2206cfb210e
Théo_CATALA <93990201+tctla@users.noreply.github.com>
2023-12-04 16:33:56 +0100

    Fetch ok

--- src/Components/PokemonList/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonList/PokemonCard/PokemonCard.jsx
@@ -1,3 +1,16 @@
 import React from "react";
 import "./PokemonCard.css";
 
+const PokemonCard = ({name, id, image, types}) => {
+   return (
+      <div className='pokemon-card'>
+        {name}
+        {/* {id} */}
+        <img className="pokemon-card-image" src={image} alt={name}/>
+        {types.map((type) => (
+           <div className={`pokemon-type ${type}`}>{type}</div>
+        ))}
+      </div>
+   );
+};
+export default PokemonCard;
--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import"./PokemonList.css";
+import React from "react";
+import "./PokemonList.css";
+import PokemonCard from "./PokemonCard/PokemonCard";
 
 const PokemonList = () => {
-  return (
-    <header>
-        <h1 className='pokemonlist' >Pokemon List</h1>
-    </header>
-  );
+   const [row, setRows] = React.useState([]);
+
+   React.useEffect(() => {
+      fetch("https://pokedex-jgabriele.vercel.app/pokemons.json")
+         .then((data) => data.json())
+         .then((json) => setRows(json || []));
+   }, []);
+   console.log(row);
+
+   return (
+      <header className='pokemonlist'>
+         {row.map((pokemon) => (
+            <PokemonCard 
+            // id={pokemon.id}
+            name={pokemon.names.fr} 
+            image={pokemon.image}
+            types={pokemon.types}
+            />
+         ))}
+      </header>
+   );
 };
 
 export default PokemonList;
