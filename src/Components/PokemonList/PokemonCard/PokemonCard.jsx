35b1c5e49b0e9da8dfa15821fc1850f1b79337fb
Théo_CATALA <93990201+tctla@users.noreply.github.com>
2023-12-04 22:00:01 +0100

    CSS Card ok

--- src/Components/PokemonList/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonList/PokemonCard/PokemonCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./PokemonCard.css";
 
-const PokemonCard = ({name, id, image, types}) => {
+const PokemonCard = ({ name, id, image, types }) => {
    return (
       <div className='pokemon-card'>
-        {name}
-        {/* {id} */}
-        <img className="pokemon-card-image" src={image} alt={name}/>
-        {types.map((type) => (
-           <div className={`pokemon-type ${type}`}>{type}</div>
-        ))}
+         <span className='id'>{id}</span>
+         <div className='pokemon-info'>
+            <span>{name}</span>
+            <img className='pokemon-card-image' src={image} alt={name} />
+            <div className='type'>
+               {types.map((type) => (
+                  <span className={`pokemon-type ${type}`}>{type}</span>
+               ))}
+            </div>
+         </div>
       </div>
    );
 };
--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -1,22 +1,32 @@
-import React from "react";
 import "./PokemonList.css";
 import PokemonCard from "./PokemonCard/PokemonCard";
+import { useState, useEffect } from "react";
 
 const PokemonList = () => {
-   const [row, setRows] = React.useState([]);
+   const [row, setRows] = useState([]);
 
-   React.useEffect(() => {
+   useEffect(() => {
       fetch("https://pokedex-jgabriele.vercel.app/pokemons.json")
          .then((data) => data.json())
          .then((json) => setRows(json || []));
    }, []);
    console.log(row);
 
+const addZeroToId = (id) => {
+   if (id < 10) {
+      return `No.00${id}`;
+   } else if (id < 100) {
+      return `No.0${id}`;
+   } else {
+      return id;
+   }
+}; 
+
    return (
       <header className='pokemonlist'>
          {row.map((pokemon) => (
             <PokemonCard 
-            // id={pokemon.id}
+            id={addZeroToId(pokemon.id)}
             name={pokemon.names.fr} 
             image={pokemon.image}
             types={pokemon.types}
